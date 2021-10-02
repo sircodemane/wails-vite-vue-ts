501744e3f3b13d1e574e7fa91dc69ae1e6e7da89
Cody Bentley <cody.bentley.la@gmail.com>
2021-10-02 10:08:45 -0600

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="logos">
+    <img class="wails" src="./assets/images/wails.svg" alt="wails">
+    <img class="vue" src="./assets/images/vue.svg" alt="vue">
+  </div>
+  <div class="links">
+    <a class="link" @click="link('https://wails.io')">Wails Docs</a> |
+    <a class="link" @click="link('https://v3.vuejs.org/guide')">Vue Docs</a> |
+    <a class="link" @click="link('https://github.com/codydbentley/wails-vite-vue-ts')">Template Docs</a>
+  </div>
+  <div class="result" id="result">{{ result }}</div>
+  <div class="input-box" id="input" data-wails-no-drag>
+    <input class="input" v-model="name" type="text" autocomplete="off">
+    <button class="btn" @click="greet()">Greet</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+// This template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from 'vue'
+
+const result = ref("Please enter your name below 👇")
+const name = ref("")
+
+const greet = () => {
+  window.go.main.App.Greet(name.value).then(response => {
+    result.value = response
+  })
+}
+
+const link = (link: string) => {
+  window.runtime.BrowserOpenURL(link)
+}
+</script>
+
+<style>
+html {
+  background-color: rgba(33, 37, 43, 1);
+  color: white;
+  font-family: "Nunito", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+  "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+  sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.links {
+  font-size: 20px;
+}
+
+.link {
+  cursor: pointer;
+  color: #df0000;
+}
+
+.link:hover {
+  text-decoration: underline;
+}
+
+@font-face {
+  font-family: "Nunito";
+  font-style: normal;
+  font-weight: 400;
+  src: local(""),
+  url("assets/fonts/nunito-v16-latin-regular.woff2") format("woff2");
+}
+
+.logos {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 50%;
+  width: 100%;
+}
+
+.logos img.wails {
+  max-width: 256px;
+  max-height: 256px;
+}
+
+.logos img.vue {
+  max-width: 256px;
+  max-height: 185px;
+}
+
+.result {
+  height: 20px;
+  line-height: 20px;
+  margin: 1.5rem auto;
+}
+
+.input-box .btn {
+  width: 60px;
+  height: 30px;
+  line-height: 30px;
+  border-radius: 3px;
+  border: none;
+  margin: 0 0 0 20px;
+  padding: 0 8px;
+  cursor: pointer;
+}
+
+.input-box .btn:hover {
+  background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
+  color: #333333;
+}
+
+.input-box .input {
+  border: none;
+  border-radius: 3px;
+  outline: none;
+  height: 30px;
+  line-height: 30px;
+  padding: 0 10px;
+  background-color: rgba(240, 240, 240, 1);
+  -webkit-font-smoothing: antialiased;
+}
+
+.input-box .input:hover {
+  border: none;
+  background-color: rgba(255, 255, 255, 1);
+}
+
+.input-box .input:focus {
+  border: none;
+  background-color: rgba(255, 255, 255, 1);
+}
+</style>
